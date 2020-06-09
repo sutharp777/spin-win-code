d95dc86f2d60e8997f08b969bc23c04f6c53c4af
sutharp777 <sutharp777@gmail.com>
2020-06-10 01:56:11 +0530

    Sound added and button validation done

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
+var prizes = {
+    count:12,
+    prize_names : ["3000 Credits", "35% Off", "Hard Luck", "70% OFF", "Swagpack", "100% OFF", "Netflix", "50% Off", "Amazon Voucher", "2 Extra Spin", "CB Tshirt", "CB Book" ]
+};
+
+
 var config = {
-    // type: Phaser.AUTO,
     width: 900,
     height: 600,
+    type: Phaser.AUTO,
     backgroundColor:0xffffff,
-    scene: {
+    pixleArt:true,
+    scene: {  
         preload: preload,
         create: create,
         update: update
@@ -23,6 +30,9 @@ function preload()
     this.load.image('pin',"./Assets/pin.png");
     this.load.image('button',"./Assets/button.png");
     this.load.image('cover',"./Assets/cover.png");
+    this.load.image('gift',"./Assets/gifts.png");
+    this.load.image('title',"./Assets/spin-n-win.png");
+    this.load.audio('spining',["./Assets/sound/spining.ogg", "./Assets/sound/spining.ogg"]);
 }
 
 function create ()
@@ -32,7 +42,7 @@ function create ()
     let W = game.config.width;
     let H = game.config.height;
 
-
+    this.spining = this.sound.add('spining');
 
     let back = this.add.sprite(W/2+x, H/2+50,'background');
     this.back = back.setScale(0.30);
@@ -48,6 +58,8 @@ function create ()
 
     let light = this.add.sprite(W/2, H/2+50,'light');
     this.light = light.setScale(1.2);
+    // this.light.scaleY = ;
+    this.light.scaleX = 1.35;
 
     back.depth = -2;
     stand.depth=-1;
@@ -55,51 +67,71 @@ function create ()
     let cover = this.add.sprite(204, H-102,'cover');
     this.cover = cover.setScale(.71);
 
+    let gift = this.add.sprite(300, H/2,'gift').setInteractive();
+    this.gift = gift.setScale(.30);
+
+    let title = this.add.sprite(300, H/2,'title').setInteractive();
+    this.title = title.setScale(.20);
+
+
     let button = this.add.sprite(200, H-100,'button').setInteractive();
     this.button = button.setScale(.21);
-    
-    // button.on("pointerdown",spinwheel, this);
+    gift.setVisible(false);
     ref = this;
+    ref.stateOfWheel = 'rest';
 
-    button.on('pointerdown', function (pointer) {
-
-        this.setTint(0xff00ff);
-        spinwheel(ref);
+    gift.on('pointerdown', function (pointer){
+        gift.setTint(0xffffff);
+        // button.setInteractive();
+        // button.setVisible(true);
+        title.setVisible(true);
+        // cover.setVisible(true);
+        gift.setVisible(false);
     });
 
+    button.on('pointerdown', function (pointer){
+
+        this.setTint(0xff00ff);
+
+        // button.disableInteractive();
+        if(ref.stateOfWheel=='rest'){
+            ref.stateOfWheel = 'move';
+            // button.setVisible(false);
+            // cover.setVisible(false);
+            spinwheel(ref);
+        }
+
+    });
     button.on('pointerout', function (pointer) {
-
         this.clearTint();
-
     });
 
     button.on('pointerup', function (pointer) {
-
         this.clearTint();
-
     });
-
-    let tweens0 = this.tweens.add({
-        targets: this.back,
-        alpha: .4,
-        ease:"Cubic.easeOut",
-        dealy:1
-    });
-
 }
 
+function resolve(){
+    ref.title.setVisible(false);
+    ref.gift.setVisible(true);
+    ref.stateOfWheel = 'rest';
+    return;
+}
 function update ()
 {
 
 }
 
 function spinwheel(ref){
+
     console.log("time to spin the wheel");
     let rounds = Phaser.Math.Between(2,4); 
 
     let extra_deg = Phaser.Math.Between(0,11)*30;
     let total_angle = rounds*360 + extra_deg; 
     ref.wheel.angle+=1300;
+
+    ref.spining.play();                // sound play
 
     let tweens = ref.tweens.add({
         targets: ref.wheel,
@@ -115,6 +147,12 @@ function spinwheel(ref){
         dealy:1,
         angle: -total_angle,
         duration:7000
+    });
+
+    ref.time.addEvent({
+        delay: 8000,
+        callback: resolve,
+        callbackScope: ref
     });
 
 }
